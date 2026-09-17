ede73721e4fa9fd39805147166c59cd6ffe12387
Sabrina Bisol <bisolsabrina778@gmail.com>
2026-09-16 21:59:59 -0300

    Implement system admin user context

--- AppProject.Core.API/Auth/UserContext.cs
+++ AppProject.Core.API/Auth/UserContext.cs
@@ -1,17 +1,34 @@
 using System;
 using AppProject.Core.Contracts;
+using AppProject.Core.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppProject.Core.API.Auth;
 
-public class UserContext : IUserContext
+public class UserContext(
+    ApplicationDbContext applicationDbContext)
+    : IUserContext
 {
     public Task<UserInfo> GetCurrentUserAsync(CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
-    public Task<UserInfo> GetSystemAdminUserAsync(CancellationToken cancellationToken = default)
+    public async Task<UserInfo> GetSystemAdminUserAsync(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var user = await applicationDbContext.Users.FirstOrDefaultAsync(u => u.IsSystemAdmin, cancellationToken);
+
+        if (user is null)
+        {
+            throw new InvalidOperationException("System admin user not found");
+        }
+
+        return new UserInfo
+        {
+            UserId = user.Id,
+            UserName = user.Name,
+            Email = user.Email,
+            IsSystemAdmin = true
+        };
     }
 }
